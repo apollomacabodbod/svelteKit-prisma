7383dd81317fba11dec3c32c1ca9fba4fb593d0b
Apollo <apollomacabodbod8@gmail.com>
2025-03-03 14:04:33 +0800

    fix: add postinstall script for prisma

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,9 +1,26 @@
-const { PrismaClient } = await import("@prisma/client");
+// const { PrismaClient } = await import("@prisma/client");
 
-const prisma = global.prisma || new PrismaClient()
+// const prisma = global.prisma || new PrismaClient()
+
+// if (process.env.NODE_ENV === "development") {
+// 	global.prisma = prisma
+// }
+
+// export { prisma }
+
+
+
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  });
 
 if (process.env.NODE_ENV === "development") {
-	global.prisma = prisma
+  globalForPrisma.prisma = prisma;
 }
 
-export { prisma }
